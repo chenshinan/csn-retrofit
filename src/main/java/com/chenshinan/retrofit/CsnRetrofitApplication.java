8df7e7515e3950e7c04f778adcdb92b831fa4857
陈士男 <shinan.chen@hand-china.com>
2019-03-03 13:40:15 +0800

    [INIT] 初始化项目

---
+++ src/main/java/com/chenshinan/retrofit/CsnRetrofitApplication.java
@@ -0,0 +1,13 @@
+package com.chenshinan.retrofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsnRetrofitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsnRetrofitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenshinan/retrofit/CsnRetrofitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chenshinan.retrofit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CsnRetrofitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
